1e3299153decebdaa77aca93e05186066aaf8950
Min Kim <mk4555@nyu.edu>
2018-02-01 21:12:55 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113163910) do
+ActiveRecord::Schema.define(version: 20180201211216) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160113163910) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.string   "post_status"
   end
 
 end
